cd8e08b9102962465fe5d21ca4d2fd6d40ff9cd9
Ezequiel Ercole <ezeerc2212@gmail.com>
2024-05-05 00:46:45 -0300

    dl

--- Assets/AnimationAndMovementController.cs
+++ Assets/AnimationAndMovementController.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class AnimationAndMovementController : MonoBehaviour
 {
@@ -17,6 +20,22 @@ public class AnimationAndMovementController : MonoBehaviour
     bool isRunPressed;
     float rotationFactorPerFrame = 5.0f;
     float runMultiplier = 3.0f;
+    float moveSpeed = 5;
+    float turnAmount = 5;
+    float turnSpeed = 10;
+
+    public static bool threeDimensions = true;
+    Vector2 moveDirection;
+    bool oneTime = false;
+
+    Vector3 position2 = new Vector3(0, 0, 0);
+    Vector3 position1 = new Vector3(0, 0, 0);
+    bool euler;
+
+    float desiredSpeed;
+    float forwardSpeed;
+    public float maxForwardSpeed = 8f;
+
 
     private void Awake()
     {
@@ -24,11 +43,11 @@ public class AnimationAndMovementController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
 
-        controls.Player.Move.started += OnMovementInput;
+        /*controls.Player.Move.started += OnMovementInput;
         controls.Player.Move.canceled += OnMovementInput;
         controls.Player.Move.performed += OnMovementInput;
         controls.Player.Run.started += OnRun;
-        controls.Player.Run.canceled += OnRun;
+        controls.Player.Run.canceled += OnRun;*/
     }
 
     void OnRun(InputAction.CallbackContext context)
@@ -36,7 +55,7 @@ public class AnimationAndMovementController : MonoBehaviour
         isRunPressed = context.ReadValueAsButton();
     }
 
-    void OnMovementInput(InputAction.CallbackContext context)
+    /*void OnMovementInput(InputAction.CallbackContext context)
     {
         currentMovementInput = context.ReadValue<Vector2>();
         currentMovement.x = currentMovementInput.x;
@@ -44,9 +63,20 @@ public class AnimationAndMovementController : MonoBehaviour
         currentRunMovement.x = currentMovementInput.x * runMultiplier;
         currentRunMovement.z = currentMovementInput.y * runMultiplier;
         isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
-    }
+    }*/
 
-    void HandleRotation()
+    void Euler()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            euler = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            euler = false;
+        }
+    }
+    /*void HandleRotation()
     {
         Vector3 positionToLookAt;
 
@@ -61,8 +91,8 @@ public class AnimationAndMovementController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
         }
 
-    }
-    void HandleAnimation()
+    }*/
+    /*void HandleAnimation()
     {
         bool isWalking = animator.GetBool("isWalking");
         bool isRunning = animator.GetBool("isRunning");
@@ -75,32 +105,88 @@ public class AnimationAndMovementController : MonoBehaviour
         {
             animator.SetBool("isWalking", false);
         }
-    }
+    }*/
 
     // Update is called once per frame
-    void Update()
+
+    private void Move(Vector2 direction)
     {
-        HandleRotation();
-        HandleAnimation();
-        if (isRunPressed)
+        float turnAmount = direction.x;
+        if (threeDimensions)
         {
-            characterController.Move(currentRunMovement * Time.deltaTime);
+            Vector3 direction2 = (direction.y * transform.forward) + (direction.x * transform.right);
+        transform.position += (direction2 * moveSpeed * Time.deltaTime);
+
+            transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+        }
+
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void Tdimesions()
+    {
+        if (threeDimensions)
+        {
+            moveDirection = controls.Player.Move.ReadValue<Vector2>();
+            if (!oneTime)
+            {
+                position2 = transform.position;
+                transform.position = new Vector3(position1.x, position2.y, position2.z);
+                oneTime = true;
+            }
         }
         else
         {
-
-            characterController.Move(currentMovement * Time.deltaTime);
+            if (euler)
+            {
+                moveDirection = controls.Player.OnMove2d.ReadValue<Vector2>();
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                if (oneTime)
+                {
+                    position1 = transform.position;
+                    transform.position = new Vector3(0, position1.y, position1.z);
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    oneTime = false;
+                }
+                transform.position = new Vector3(0, transform.position.y, transform.position.z);
+            }
+            if (!euler)
+            {
+                moveDirection = controls.Player.OnMove2d1.ReadValue<Vector2>();
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                if (oneTime)
+                {
+                    position1 = transform.position;
+                    transform.position = new Vector3(0, position1.y, position1.z);
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    oneTime = false;
+                }
+                transform.position = new Vector3(0, transform.position.y, transform.position.z);
+            }
         }
 
     }
 
-    void OnEnable()
+    void Update()
     {
-        controls.Player.Enable();
-    }
+        isMovementPressed = moveDirection.x != 0 || moveDirection.y != 0;
+        //HandleRotation();
+        //onGround = IsGrounded();
+        Tdimesions();
+        Move(moveDirection);
+        Euler();
 
-    void OnDisable()
-    {
-        controls.Player.Disable();
+        //HandleAnimation();
+        /*       if (isRunPressed)
+               {
+                   characterController.Move(currentRunMovement * Time.deltaTime);
+               }
+               else
+               {
+                   Move(currentMovement);
+               }*/
     }
 }
