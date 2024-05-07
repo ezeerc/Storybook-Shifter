0af58d45f1b8802170fa66d68189d36753a8a5ba
Ezequiel Ercole <ezeerc2212@gmail.com>
2024-05-06 22:22:59 -0300

    versión previa a la final

--- Assets/Scripts/LaberintoS.cs
+++ Assets/Scripts/LaberintoS.cs
@@ -9,7 +9,17 @@ public class LaberintoS : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Destroy(this.gameObject);
+            //DisabePlatform();
         }
+    }
+
+    private IEnumerator DisabePlatform()
+    {
+        this.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(3);
+
+        this.gameObject.SetActive(true);
     }
 
 
